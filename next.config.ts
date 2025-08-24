c5b7b3162943e7628ab45e3448ed4c30e9a128cf
CodewithEVILXD <126612139+CodewithEvilxd@users.noreply.github.com>
2025-08-24 17:13:01 +0530

    xd

--- next.config.ts
+++ next.config.ts
@@ -3,28 +3,6 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  cacheId: "lexigenius-pwa",
-  manifest: {
-    name: "LexiGenius",
-    short_name: "LexiGenius",
-    description: "An AI-powered writing assistant.",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
-    icons: [
-      {
-        src: "/image.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/image.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
 });
 
 /** @type {import('next').NextConfig} */
